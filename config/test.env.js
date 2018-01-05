42d1fdd885ba4b10ed6255488e04ee32481208f1
TeruyukiIshii <TeruyukiIshii@gmail.com>
2018-01-05 21:58:31 +0900

    init

---
+++ config/test.env.js
@@ -0,0 +1,8 @@
+'use strict'
+const merge = require('webpack-merge')
+const devEnv = require('./dev.env')
+
+module.exports = merge(devEnv, {
+  NODE_ENV: '"testing"',
+  API_URL: '"https://rails-api-sample01-stg.herokuapp.com/"'
+})
---
+++ src/components/Top.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="top">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Top',
+  data () {
+    return {
+      msg: process.env.API_URL
+    }
+  }
+}
+</script>
